dbf5fda708be31b74359879be9fe0a5770f7d0e3
erick <erickvogel@me.com>
2017-05-15 00:24:06 -0700

    Add files via upload

--- final_assignment_v2/js/main.js
+++ final_assignment_v2/js/main.js
@@ -31,7 +31,7 @@ $(document).ready(function(){
 					'<div class="column">'+
 						'<div class="module">'+
 							'<img src="'+element.urlToImage+'"></img>'+
-							'<h2 id="title"><a href="url: "https://www.theverge.com/2017/5/13/15635052/king-arthur-legend-of-the-sword-review-guy-ritchie-charlie-hunnam",">'+element.title+'</a></h2>'+
+							'<h2 id="title"><a href="'+element.url+'">'+element.title+'</a></h2>'+
 							'<h3 id="author">'+element.author+'</h3>'+
 							'<p id="description">'+element.description+'</p>'+
 						'</div>'+
@@ -42,7 +42,7 @@ $(document).ready(function(){
 			}
 		});
 	}
-});
+
 
 
 generateModules()
@@ -58,8 +58,8 @@ function generateModules(){
     var color = $('#colors').val();
 
     //let's go through our conditions to set the background!
-    if(color == 'gray') {
-      $('body').attr('id','gray');
+    if(color == 'grey') {
+      $('body').attr('id','grey');
     }
     else if (color == 'white') {
       $('body').attr('id','white');
@@ -76,5 +76,32 @@ function generateModules(){
   });
 
 
+// Get the modal
+var modal = document.getElementById('myModal');
 
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
+
+
+});
 
