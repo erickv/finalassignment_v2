013a49f155ae0672f127759be95eb72805de7b37
erick <erick.whitemonky@gmail.com>
2017-05-15 21:17:35 -0700

    delete the extra pages modal.js and styles.css
    delete the extra pages modal.js and styles.css

---
+++ js/test.js
@@ -0,0 +1,15 @@
+$(window).scroll(function(){
+	var scrollTop = $(window).scrollTop();
+	var docHeight = $(document).height();
+	var scrollHeight = $(window).height();
+
+if (scrollTop >= (docHeight - winHeight) -2){
+	console.log
+	searchGiphy();	
+
+}
+
+
+
+
+})	
